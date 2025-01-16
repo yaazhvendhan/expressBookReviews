6db8085ea2e6e7bf8a1e6d7b364a8b9f4693934a
yaazhvendhan <yaazhvendhanms@gmail.com>
2025-01-16 08:07:56 -0500

    created and modified bookreview

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,18 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+const token = req.headers['authorization']?.split(' ')[1]; // Extract token from Authorization header
+if (!token) {
+    return res.status(401).send('Access Denied');
+}
+
+jwt.verify(token, 'your_jwt_secret', (err, decoded) => {
+    if (err) {
+        return res.status(403).send('Invalid token');
+    }
+    req.user = decoded; // Attach user data to request
+    next(); // Proceed to the next middleware
+});
 });
  
 const PORT =5000;
