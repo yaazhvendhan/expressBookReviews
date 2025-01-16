ddf36ede5fcff2896a71d89d6cf86906acc8c81f
yaazhvendhan <yaazhvendhanms@gmail.com>
2025-01-16 08:22:24 -0500

    modified author_user.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,85 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (user)=>{ //returns boolean
+    let filtered_users = users.filter((user)=> user.username === user);
+    if(filtered_users){
+        return true;
+    }
+    return false;
+}
+const authenticatedUser = (username,password)=>{ //returns boolean
+    if(isValid(username)){
+        let filtered_users = users.filter((user)=> (user.username===username)&&(user.password===password));
+        if(filtered_users){
+            return true;
+        }
+        return false;
+       
+    }
+    return false;
+    
+
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+regd_users.post("/register", (req,res) => {
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+    if(username&&password){
+        const present = users.filter((user)=> user.username === username)
+        if(present.length===0){
+            users.push({"username":req.body.username,"password":req.body.password});
+            return res.status(201).json({message:"USer Created successfully"})
+        }
+        else{
+          return res.status(400).json({message:"Already exists"})
+        }
+    }
+    else if(!username && !password){
+      return res.status(400).json({message:"Bad request"})
+    }
+    else if(!username || !password){
+      return res.status(400).json({message:"Check username and password"})
+    }
+  
+   
+  });
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let user = req.body.username;
+    let pass = req.body.password;
+    if(!authenticatedUser(user,pass)){
+        return res.status(403).json({message:"User not authenticated"})
+    }
+
+    let accessToken = jwt.sign({
+        data: user
+    },'access',{expiresIn:60*60})
+    req.session.authorization = {
+        accessToken
+    }
+    res.send("User logged in Successfully")
+ 
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let userd = req.session.username;
+  let ISBN = req.params.isbn;
+  let details = req.query.review;
+  let rev = {user:userd,review:details}
+  books[ISBN].reviews = rev;
+  return res.status(201).json({message:"Review added successfully"})
+  
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let ISBN = req.params.isbn;
+    books[ISBN].reviews = {}
+    return res.status(200).json({messsage:"Review has been deleted"})
 });
 
 module.exports.authenticated = regd_users;
